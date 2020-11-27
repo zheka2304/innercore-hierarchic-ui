6b7cac18b3031d283a65c9ad070abf8d90bea440
Eugene Smirnov <zheka2304@gmail.com>
2020-11-27 16:49:39 +0300

    added button view

---
+++ view/description/button.js
@@ -0,0 +1,12 @@
+// alignment logic is same as image view
+class UiButtonView extends UiImageView {
+    parse(description) {
+        description = super.parse(description);
+        description.bitmap2 = description.bitmap2 || description.bitmapPressed;
+    }
+
+    render() {
+        return { ...this.description, type: "button" };
+    }
+}
+
